d8e4510878c0cbcc022dd8dd20d1f2578c430e9c
ShengruiZhang <msi1459481096@gmail.com>
2017-09-25 11:00:17 -0700

    HW4P1 upadate
    Now the problem is finally working

--- Hw4_problems/Hw4/hw4p1.c
+++ Hw4_problems/Hw4/hw4p1.c
@@ -14,14 +14,12 @@ Outputs: Fishes or mice
 
 // Nested loop is required to use
 
-
+// A fish looks like this: ><(((('>
+// A mouse looks like this: ----{,_,<">
 
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <stdio.h>
-
-// A fish looks like this: ><(((('>
-// A mouse looks like this: ----{,_,<">
 
 void PRINT_FISH(void) {  // Funtion that prints the fish, save some work probably?
 	printf("><(((('>\n");
@@ -40,39 +38,19 @@ int main(void) {
 	
 	unsigned int amount = 0; // to determine how many ----{,_,<"> ><(((('> get display, also how many column it is
 	
-	//printf("This program prints\n");
-	//printf("1) a school of fish or\n");
-	//printf("2) mice in a maze.\n");
-
-	//while (type != 1 && type != 2) {
-	//	printf("Enter your option (1 or 2):");   // 1 for fish, 2 for mouse
-	//	scanf(" %d", &type);
-	//	//printf("%d\n", type);
-	//}
-
-	//printf("How many fish are in the school?");
-	//scanf(" %d", &amount);
-	//printf("%d\n", amount);
-	
-	//PRINT_FISH();
-	//PRINT_MICE();
-	
-	
 	// Printing
-	
-	
-	
-	unsigned char quit = '?'; // quit or not?
+		
+	//unsigned char quit = '?'; // quit or not?
 	
 	unsigned int line = 0; // to indicate the line
 	
 	unsigned int o = 0; // outer loop variable
 	
-	unsigned int i = 0; // inner loop variable, number of tab printed
+	unsigned int i = 0; // inner loop variable, number of tab printf
 	
-	unsigned int i_2 = 0;
+	unsigned int i_2 = 0; // secon inner loop variable
 	
-	while (quit != 'N' || quit != 'n') { // Type N or n causes program to terminate
+	//while (quit != 'N' && quit != 'n') { // Type N or n causes program to terminate
 		
 		line = 0; // reset line counter
 		type = 0; // reset type
@@ -81,48 +59,126 @@ int main(void) {
 		printf("1) a school of fish or\n");
 		printf("2) mice in a maze.\n");
 
-		while (type != 1 && type != 2) {
-			printf("Enter your option (1 or 2):");   // 1 for fish, 2 for mouse
+		while (type != 1 && type != 2) { // Take the valid input
+			printf("Enter your option (1 or 2)");   // 1 for fish, 2 for mouse
 			scanf(" %d", &type);
-			//printf("%d\n", type);
+			printf("\n");
 		}
 
-		printf("How many fish are in the school?");
-		scanf(" %d", &amount);
-		//printf("%d\n", amount);
+		switch (type)
+		{
+		case (1):
+			printf("How many fish are in the school? ");
+			scanf(" %d", &amount);
+			printf("\n");
+			break;
+		case (2):
+			printf("How many mice in the maze? ");
+			scanf(" %d", &amount);
+			printf("\n");
+		default:
+			break;
+		}
 
-		do {   // Print a fish first or print tab first until enough
+		printf("\n");
+		
+		do {   // Print a fish first or printf tab first until enough
 			
-			switch (type) {
+			switch (type)
+			{
 				
-				case 1:    // Print fish
-				if (line < (amount / 2)) { // print the first part of fish when line reachs half of amount
-					PRINT_FISH();
-					line++;
-					for (i = 0; i < line; i++) { // Print tabs
-						printf("\t");
+				case 1:                                                        // Print fish
+				if ((amount % 2) == 0) {                                // even amount
+					for (o = 0; o <= ((amount / 2) - 1); o++) {
+						for (i = 0; i < line; i++) {
+							printf("\t");
+						}
+						PRINT_FISH();
+						line++;
+					}
+
+					for (i_2 = 0; i_2 < (amount / 2); i_2++) {
+						for (o = 0; o < i; o++) {
+							printf("\t");
+						}
+						i--;
+						PRINT_FISH();
+						line++;
 					}
 				}
-				else if (line >= (amount / 2)) { 
-					PRINT_FISH();
-					line++;
-					for (i_2 = 0; i_2 < i; i_2++) {
-						printf("\t");
+				else if ((amount % 2) != 0) {                            // odd amount
+					for (o = 0; o < ((amount / 2) + 1); o++) {           // first part
+						for (i = 0; i < line; i++) {
+							printf("\t");
+						}
+						PRINT_FISH();
+						line++;
+					}
+					
+					for (i_2 = 0; i_2 < (amount / 2); i_2++) {           // second part
+						for (o = 0; o < (i - 1); o++) {
+							printf("\t");
+						}
 						i--;
+						PRINT_FISH();
+						line++;
 					}
 				}
 				break;
 				
-				case 2:    // Print first
-				printf("\t");
+				case 2:                                                         // Print mouse
+				if ((amount % 2) == 0) {                                // even amount
+						i_2 = (amount / 2) - 1;
+						for (o = 0; o < ((amount / 2) - 0); o++) {
+							for (i = 0; i < i_2; i++) {
+								printf("\t");
+							}
+							i_2--;
+							PRINT_MICE();
+							line++;
+						}
+
+						i = 0;
+						for (i_2 = 0; i_2 < (amount / 2); i_2++) {
+							for (o = 0; o < i; o++) {
+								printf("\t");
+							}
+							i++;
+							PRINT_MICE();
+							line++;
+						}
+					}
+				else if ((amount % 2) != 0) {                            // odd amount
+						i_2 = (amount / 2);
+						for (o = 0; o < ((amount / 2) + 1); o++) {           // first part
+							for (i = 0; i < (i_2); i++) {
+								printf("\t");
+							}
+							i_2--;
+							PRINT_MICE();
+							line++;
+						}
+
+						i = 1;
+						for (i_2 = 0; i_2 < (amount / 2); i_2++) {           // second part
+							for (o = 0; o < i; o++) {
+								printf("\t");
+							}
+							i++;
+							PRINT_MICE();
+							line++;
+						}
+					}
 				break;
 			}
 			
-		} while (line < (amount - 1)); // use line to print objects
+		} while (line < (amount - 1)); // use line to printf objects
 
-		printf("Would you like to continue.(N/n to quit):");
-		scanf(" %c", &quit);
-	}
+		//printf("\nWould you like to continue.(N/n to quit):");
+		printf("\n");
+		//scanf(" %c", &quit);
+		//printf("\n");
+	//}
 	
 	return 0;
 }
